34d86f975cd98c3666f6cae9a76ba59acd896334
GabS <gabrielsoares1602@gmail.com>
2024-01-15 00:57:22 -0300

    image sheet organized

--- src/embedMessage/showSheetEmbed.js
+++ src/embedMessage/showSheetEmbed.js
@@ -7,16 +7,16 @@ function showSheetEmbed(characterSheet) {
     .setColor(characterSheet.color)
     .addFields(
 
-        { name: `Vida:`, value: `${characterSheet.hp.current}/${characterSheet.hp.max}`, inline:true},
-        { name: `PE:`, value: `${characterSheet.mana.current}/${characterSheet.mana.max}`, inline:true},
-        { name: `Sanidade:`, value: `${characterSheet.sanity.current}/${characterSheet.sanity.max}`, inline:true},
+        createCharacterStat('Vida', characterSheet.hp),
+        createCharacterStat('PE', characterSheet.pe),
+        createCharacterStat('Sanidade', characterSheet.sanity),
 
         { name: `NEX: ${characterSheet.level}%`, value: `Max PE: ${characterSheet.level / 5}`, inline: true},
         
         { name: 'Origem:', value: characterSheet.origin, inline: true },
 		{ name: 'Classe:', value: characterSheet.class, inline: true },
 		{ name: 'Deslocamento:', value: characterSheet.mobility, inline: true},
-		{ name: 'Defesa:', value: `${characterSheet.inventory.defense.kevlar.def + 10}`, inline: true},
+		{ name: 'Defesa:', value: calcDef(characterSheet), inline: true},
 
         { name: 'Atributos:', value: formatAttributes(characterSheet.attributes)},
         { name: 'Ataques:', value: formatWeapons(characterSheet.inventory.weapons), inline: true}
@@ -33,7 +33,19 @@ function formatAttributes(attributes){
 }
 
 function formatWeapons(weapons){
-    return Object.entries(weapons).map(([name, weapons]) => `**${weapons.nome}**/${weapons.alcance}\ndano: ${weapons.dano}/${weapons.critico_dado, weapons.critico_multi}`).join('\n')
+    return Object.entries(weapons).map(([name, weapons]) => `**${weapons.nome}** / ${weapons.alcance}\ndano: ${weapons.dano} / ${weapons.critico_dado, weapons.critico_multi}`).join('\n')
 }
 
-export default showSheetEmbed
+function createCharacterStat(name, stat){
+    return {
+        name: `${name}:`,
+        value: `${stat.current}/${stat.max}`,
+        inline: true
+    }
+}
+
+function calcDef(sheet){
+    return `${sheet.inventory.defense.kevlar.def + sheet.skills_text.AGI + 10}`
+}
+
+export default showSheetEmbed;
